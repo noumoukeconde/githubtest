bb004088a8bde5e59c3fe10620ea3bf9ede3fb2f
noumoukeconde <noumoukeconde@gmail.com>
2024-08-22 20:56:58 -0400

    Create br.py
    practice if statement

---
+++ ChuckCourses/br.py
@@ -0,0 +1,10 @@
+#coding RB robot barrista
+
+print("Hello, welcome to NoumNet !!!!")
+
+name = input("What is your name?\n")
+
+if name == "Ben":
+    print("You're not welcome here Evil Ben!! Get out!!")
+else:
+    print("Hello " + name + ", thank you so much for coming in today. \n\n\n")
